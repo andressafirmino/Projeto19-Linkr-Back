8c5caa582dfe7061fecf2b67515176ae979d6e2b
gustavohubnerc <hubnergc@gmail.com>
2023-08-15 10:18:46 -0300

    fix: sign up schema

--- src/schemas/signup.schemas.js
+++ src/schemas/signup.schemas.js
@@ -2,7 +2,7 @@ import Joi from "joi";
 
 export const signupSchema = Joi.object({
     username: Joi.string().required(),
-    email: joi.string().required().email(),
-    image: joi.string().required(),
-    password: joi.string().required().min(8)
+    email: Joi.string().required().email(),
+    image: Joi.string().required(),
+    password: Joi.string().required().min(8)
 });
