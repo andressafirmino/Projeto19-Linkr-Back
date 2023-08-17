c12ce173e22f3134ab79a79b0692c8b1e7ab991b
Quillinan <milton.quillinan@gmail.com>
2023-08-16 22:16:07 -0300

    fix: getPostsFromUser

--- src/repositories/posts.repository.js
+++ src/repositories/posts.repository.js
@@ -1,23 +1,32 @@
 import { db } from "../database/database.connection.js";
 
 export async function publicPost(link, description, userId) {
-    const result = db.query(`
+  const result = db.query(
+    `
     INSERT INTO posts (link, description, "userId") VALUES ($1, $2, $3)
     RETURNING "id";
-    `, [link, description, userId]);
-    return result;
+    `,
+    [link, description, userId]
+  );
+  return result;
 }
 
 export async function publicHasthtag(name) {
-    const result = db.query(`
+  const result = db.query(
+    `
     INSERT INTO hashtags (name) VALUES ($1) RETURNING "id";
-    `, [name]);
-    return result;
+    `,
+    [name]
+  );
+  return result;
 }
 
 export async function postTags(postId, tagId) {
-    const result = db.query(`
+  const result = db.query(
+    `
     INSERT INTO post_hashtags ("postId", "tagId") VALUES ($1, $2);
-    `, [postId, tagId]);
-    return result;
+    `,
+    [postId, tagId]
+  );
+  return result;
 }
